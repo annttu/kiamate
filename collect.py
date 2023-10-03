43b286c1cdbde78be14f8df1fddd906a4ab4032e
Antti Jaakkola <annttu@git.annttu.fi>
2023-10-03 09:01:35 +0300

    Initial version

---
+++ collect.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+
+from hyundai_kia_connect_api import *
+import time
+import pprint
+import logging
+
+
+import credentials
+import database
+
+logger = logging.getLogger("kia-mate")
+
+pp = pprint.PrettyPrinter(indent=4, depth=4, width=80)
+
+
+def add_or_update_car(vehicle):
+    q = database.Car.query.filter(database.Car.api_id == vehicle.id)
+    car_obj = q.first()
+    if not car_obj:
+        print("Creating new car")
+        car_obj = database.Car(
+            api_id=vehicle.id,
+            name=vehicle.name,
+            model=vehicle.model,
+        )
+        database.db_session.add(car_obj)
+    if car_obj.name != vehicle.name:
+        print("Updating car name")
+        car_obj.name = vehicle.name
+        database.db_session.add(car_obj)
+    database.db_session.commit()
+    return car_obj.id
+
+
+def update_location(car_id, vehicle):
+    q = database.Location.query.filter(
+        database.Location.car_id == car_id,
+        database.Location.time == vehicle.last_updated_at)
+    location_obj = q.first()
+    if location_obj:
+        return
+    location_obj = database.Location(
+        car_id=car_id,
+        lat=vehicle.location_latitude,
+        lon=vehicle.location_longitude,
+        speed=vehicle.data.get('vehicleLocation', {}).get('speed', {}).get('value', 0),
+        heading=vehicle.data.get('vehicleLocation', {}).get('head', 0),
+        odo=vehicle.odometer,
+        # location_last_updated_at timestamp timezone and date don't match...
+        # time=vehicle.location_last_updated_at
+        time=vehicle.last_updated_at
+    )
+    database.db_session.add(location_obj)
+    database.db_session.commit()
+
+
+def update_ev_battery(car_id, vehicle):
+    q = database.EVBattery.query.filter(
+        database.EVBattery.car_id == car_id,
+        database.EVBattery.time == vehicle.last_updated_at)
+    db_obj = q.first()
+    if db_obj:
+        return
+    db_obj = database.EVBattery(
+        car_id=car_id,
+        battery_charging=int(vehicle.ev_battery_is_charging),
+        battery_percent=vehicle.ev_battery_percentage,
+        time=vehicle.last_updated_at,
+    )
+    database.db_session.add(db_obj)
+    database.db_session.commit()
+
+
+def update_ev_range(car_id, vehicle):
+    q = database.EVRange.query.filter(
+        database.EVRange.car_id == car_id,
+        database.EVRange.time == vehicle.last_updated_at)
+    db_obj = q.first()
+    if db_obj:
+        return
+    db_obj = database.EVRange(
+        car_id=car_id,
+        ev_range=int(vehicle.ev_driving_range),
+        time=vehicle.last_updated_at,
+    )
+    database.db_session.add(db_obj)
+    database.db_session.commit()
+
+
+def update_daily_stats(car_id, vehicle):
+    for stat in vehicle.daily_stats:
+        update_daily_stat(car_id=car_id, stat=stat)
+    database.db_session.commit()
+
+
+def update_daily_stat(car_id, stat):
+    q = database.DailyStats.query.filter(
+        database.DailyStats.car_id == car_id,
+        database.DailyStats.time == stat.date)
+    db_obj = q.first()
+    if db_obj:
+        return
+    db_obj = database.DailyStats(
+        car_id=car_id,
+        total_consumed=stat.total_consumed,
+        engine_consumption=stat.engine_consumption,
+        climate_consumption=stat.climate_consumption,
+        onboard_electronics_consumption=stat.onboard_electronics_consumption,
+        battery_care_consumption=stat.battery_care_consumption,
+        regenerated_energy=stat.regenerated_energy,
+        distance=stat.distance,
+        time=stat.date,
+    )
+    database.db_session.add(db_obj)
+
+
+def main():
+    database.init_db()
+
+    vm = VehicleManager(region=1, brand=1, username=credentials.email, password=credentials.password,
+                        pin=credentials.pin)
+
+    vm.check_and_refresh_token()
+
+    vm.update_all_vehicles_with_cached_state()
+
+    while True:
+        start = time.time()
+        for vehicle_id, vehicle in vm.vehicles.items():
+            pprint.pprint(vehicle)
+            print(vehicle.id, vehicle.name)
+            try:
+                car_id = add_or_update_car(vehicle=vehicle)
+            except Exception:
+                logger.exception("Failed to store vehicle state")
+                database.db_session.rollback()
+                continue
+            try:
+                update_location(car_id=car_id, vehicle=vehicle)
+            except Exception:
+                logger.exception("Failed to store vehicle state")
+                database.db_session.rollback()
+            try:
+                update_ev_battery(car_id=car_id, vehicle=vehicle)
+            except Exception:
+                logger.exception("Failed to store vehicle state")
+                database.db_session.rollback()
+            try:
+                update_ev_range(car_id=car_id, vehicle=vehicle)
+            except Exception:
+                logger.exception("Failed to store vehicle state")
+                database.db_session.rollback()
+            try:
+                update_daily_stats(car_id=car_id, vehicle=vehicle)
+            except Exception:
+                logger.exception("Failed to store vehicle state")
+                database.db_session.rollback()
+
+        time.sleep(int(start + 900 - time.time()))
+
+        try:
+            vm.check_and_force_update_vehicles(force_refresh_interval=60)
+        except Exception:
+            logger.exception("Failed to update vehicle states")
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    main()
---
+++ database.py
@@ -0,0 +1,84 @@
+from sqlalchemy import create_engine, Column, Integer, DateTime, \
+     ForeignKey, Float, UniqueConstraint
+from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
+from sqlalchemy.dialects import postgresql
+
+
+import credentials
+
+engine = create_engine(credentials.database_uri)
+db_session = scoped_session(
+        sessionmaker(autocommit=False, autoflush=False, bind=engine)
+)
+
+
+def init_db():
+    Model.metadata.create_all(bind=engine)
+
+
+Model = declarative_base(name='Model')
+Model.query = db_session.query_property()
+
+
+class Car(Model): 
+    __tablename__ = 'car'
+    id = Column('id', Integer, primary_key=True, nullable=False, unique=True)
+    api_id = Column('api_id', postgresql.TEXT, nullable=False, unique=True)
+    name = Column('name', postgresql.TEXT, nullable=False)
+    model = Column('model', postgresql.TEXT, nullable=False)
+
+
+class Location(Model):
+    __tablename__ = 'location'
+
+    id = Column('id', postgresql.BIGINT, primary_key=True, nullable=False, unique=True)
+    car_id = Column('car_id', Integer, ForeignKey('car.id'), nullable=False)
+    lat = Column('lat', Float, nullable=False)
+    lon = Column('lon', Float, nullable=False)
+    speed = Column('speed', Integer, nullable=False)
+    heading = Column('heading', Integer, nullable=False)
+    odo = Column('odo', Float, nullable=True)
+    time = Column('time', DateTime(timezone=True), nullable=False)
+
+    UniqueConstraint(car_id, time)
+
+
+class EVBattery(Model):
+    __tablename__ = 'ev_battery'
+
+    id = Column('id', postgresql.BIGINT, primary_key=True, nullable=False, unique=True)
+    car_id = Column('car_id', Integer, ForeignKey('car.id'), nullable=False)
+    battery_charging = Column('battery_charging', Integer, nullable=False)
+    battery_percent = Column('battery_percent', Integer, nullable=False)
+    time = Column('time', DateTime(timezone=True), nullable=False)
+
+    UniqueConstraint(car_id, time)
+
+
+class EVRange(Model):
+    __tablename__ = 'ev_range'
+
+    id = Column('id', postgresql.BIGINT, primary_key=True, nullable=False, unique=True)
+    car_id = Column('car_id', Integer, ForeignKey('car.id'), nullable=False)
+    ev_range = Column('range', Integer, nullable=False)
+    time = Column('time', DateTime(timezone=True), nullable=False)
+
+    UniqueConstraint(car_id, time)
+
+
+class DailyStats(Model):
+    __tablename__ = 'daily_stats'
+
+    id = Column('id', postgresql.BIGINT, primary_key=True, nullable=False, unique=True)
+    car_id = Column('car_id', Integer, ForeignKey('car.id'), nullable=False)
+    total_consumed = Column('total_consumed', Integer, nullable=False)
+    engine_consumption = Column('engine_consumption', Integer, nullable=False)
+    climate_consumption = Column('climate_consumption', Integer, nullable=False)
+    onboard_electronics_consumption = Column('onboard_electronics_consumption', Integer, nullable=False)
+    battery_care_consumption = Column('battery_care_consumption', Integer, nullable=False)
+    regenerated_energy = Column('regenerated_energy', Integer, nullable=False)
+    distance = Column('distance', Integer, nullable=False)
+
+    time = Column('time', DateTime(timezone=True), nullable=False)
+
+    UniqueConstraint(car_id, time)
